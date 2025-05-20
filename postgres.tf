2522ee1bcea7ab5ad37fca04690adf20f848e69f
anne-moore <Anne.Moore@justice.gov.uk>
2025-05-20 16:44:26 +0100

    Updating keyvault name

--- postgres.tf
+++ postgres.tf
@@ -1,6 +1,6 @@
 module "key_vault" {
   source              = "git@github.com:hmcts/cnp-module-key-vault?ref=master"
-  name                = "sds-platform-sbox" // Max 24 characters
+  name                = "sds-platform-test-sbox" // Max 24 characters
   product             = var.product
   env                 = var.env
   object_id           = var.jenkins_AAD_objectId
